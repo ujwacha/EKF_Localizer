a2ba9273823917c9abeda06cb548b3d94328081b
ujwacha <ujwolacharya4@gmail.com>
2024-12-22 21:47:01 +0545

    Kalman Tested, seems pretty bad though

---
+++ data/kfplot.gp
@@ -0,0 +1,21 @@
+set xlabel "X-axis"
+set ylabel "Y-axis"
+set grid
+
+# Specify the delimiter as a comma (CSV file)
+set datafile separator ","
+
+# Animate point
+n = system("wc -l < data.csv")  # Count the number of lines in the CSV file
+
+do for [i=1:n] {
+    plot "data.csv" using 5:6 every ::1::i with lines title "Locus", \
+         "data.csv" using 5:6 every ::i::i with points pointtype 7 pointsize 1 title sprintf("Frame %d", i)
+    # plot "data.csv" using 1:4 every ::1::i with linespoints title "Angle"
+
+
+    pause 0.1  # Adjust delay (in seconds) between frames
+    
+}
+
+pause -1 "Press any key to close the window..."
--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,11 @@
 
 #include "kalman/ExtendedKalmanFilter.hpp"
 
+#include <iostream>
+#include <random>
+#include <chrono>
+
+
 
 typedef float T;
 
@@ -39,15 +44,24 @@ int main() {
   ImuMeasurementModel imu_model;
   SystemModel sys;
 
-  //     // Random number generation (for noise simulation)
-  // std::default_random_engine generator;
-  // generator.seed( std::chrono::system_clock::now().time_since_epoch().count() );
-  // std::normal_distribution<T> noise(0, 1);
+
+  T SystemNoise = 0.03;
+  T OdomNoise = 0.1;
+  T ImuNoise = 0.06;
+    
+
+      // Random number generation (for noise simulation)
+  std::default_random_engine generator;
+  generator.seed( std::chrono::system_clock::now().time_since_epoch().count() );
+  std::normal_distribution<T> noise(0, 1);
   
  
   Kalman::ExtendedKalmanFilter<State> predictor;
+  Kalman::ExtendedKalmanFilter<State> ekf;
 
   predictor.init(state);
+  ekf.init(state);
+
 
   twist.rvx() = 1;
   twist.rvy() = 2;
@@ -57,7 +71,55 @@ int main() {
   for (int i = 0; i < 300; i++) {
     state = sys.f(state, twist, 0.05);
 
-    fs << i << "," << state.x() << "," << state.y() << "," << state.theta() << std::endl;
+    state.x() += SystemNoise*noise(generator);
+    state.y() += SystemNoise*noise(generator);
+    state.theta() += SystemNoise*noise(generator);
+    state.vx() += SystemNoise*noise(generator);
+    state.vy() += SystemNoise*noise(generator);
+    state.ax() += SystemNoise*noise(generator);
+    state.ay() += SystemNoise*noise(generator);
+
+
+    auto x_ekf = ekf.predict(sys, twist, 0.05);
+    auto x_pred = predictor.predict(sys, twist, 0.05);
+
+
+
+
+        // Odom measurement
+        {
+            // We can measure the orientation every 5th step
+            OdomMeasurement odom = odom_model.h(state);
+            // Measurement is affected by noise as well
+            odom.theta() += OdomNoise * noise(generator);
+            odom.omega() += OdomNoise * noise(generator);
+            odom.x() += OdomNoise * noise(generator);
+            odom.y() += OdomNoise * noise(generator);
+            odom.vx() += OdomNoise * noise(generator);
+            odom.vy() += OdomNoise * noise(generator);
+
+            // Update EKF
+            x_ekf = ekf.update(odom_model, odom, 0.05);
+            
+        }
+
+	// Imu Measurement
+        {
+            // We can measure the orientation every 5th step
+            ImuMeasurement imu = imu_model.h(state);
+            // Measurement is affected by noise as well
+            imu.yaw() += ImuNoise * noise(generator);
+            imu.ax() += ImuNoise * noise(generator);
+            imu.ay() += ImuNoise * noise(generator);
+
+            // Update EKF
+            x_ekf = ekf.update(imu_model, imu, 0.05);
+            
+        }
+
+
+
+	fs << i << "," << state.x() << "," << state.y() << "," << state.theta() << "," << x_ekf.x() << "," << x_ekf.y() << "," << x_ekf.theta() << std::endl;
     // Should Have Used Case Statement Lol
     if (i  == 70) {
       std::cout << "OPP" << std::endl;
@@ -78,7 +140,7 @@ int main() {
     if ( i==150 ) {
       twist.rvx() = 0;
       twist.rvy() = 0;
-      twist.romega() = 0;
+      twist.romega() = 1;
     }
 
     if (i==200) {
